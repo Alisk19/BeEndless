b89213d4f838c797c1a286f38f4128f50500d321
Alisk19 <beendless44@gmail.com>
2026-03-05 12:42:06 +0530

    chore: remove quotation html and update configurations for production

--- client/services/firebase.ts
+++ client/services/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC88UptgjQ5JR9uvyt5fR0kYUKjoCKtRgk",
-    authDomain: "be-endless.firebaseapp.com",
-    projectId: "be-endless",
-    storageBucket: "be-endless.firebasestorage.app",
-    messagingSenderId: "194215034873",
-    appId: "1:194215034873:web:91aea4c314bbf1900b8728",
-    measurementId: "G-EH2HJDYPWZ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
